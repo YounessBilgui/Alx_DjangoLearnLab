8c153e05f161ddf198d3d5a5e374bce44468aab4
Youness <younessbilgui007@gmail.com>
2025-07-28 12:50:11 +0100

    fix : AUTH_USER_MODEL settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 # Custom User Model
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
